283f443f11e61634290d6c98777ae9b5928c8c1b
jlange <jlange54@gmx.de>
2023-12-24 22:16:43 +0100

    Import Regex class & add new match functionality

---
+++ src/Regex.java
@@ -0,0 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static String regexForReferenceLongitude = "0,ReferenceLongitude=(.*)";
+    public static String regexForReferenceLatitude = "0,ReferenceLatitude=(.*)";
+    public static String regexForReferenceTime = "0,ReferenceTime=(.*)Z";
+    public static String regexForRecordingTime = "0,RecordingTime=(.*)Z";
+    public static String regexForTitle = "0,Title=(.*)";
+    public static String regexForDataSource = "0,DataSource=(.*)";
+    public static String regexForAuthor = "0,Author=(.*)";
+    public static String substitution(String line, String regex, int returnGroup) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()){
+            return matcher.group(returnGroup);
+        }
+        return null;
+    }
+
+    public static boolean match (String line, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()){
+            return true;
+        }
+        return false;
+    }
+}
