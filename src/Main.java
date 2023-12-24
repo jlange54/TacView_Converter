2b02f2c966b73478e760a57512ccf669da52503e
jlange <jlange54@gmx.de>
2023-12-24 22:21:46 +0100

    Test main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Runtime.start();
+
+        System.out.println(getMetaData.fromFile("./testFiles/Tacview-20231224-123223-DCS-refuelTrainingF18.zip.txt.acmi"));
+
+
+        System.out.println("Program completed in " + Runtime.end(Runtime.timeUnit.ms) + " ms");
     }
 }
